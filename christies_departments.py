2cf4c12d2b9f702eefe6aa471425ff9910d82288
Marco Meneghelli <mm@bendingspoons.com>
2018-12-13 12:14:00 +0100

    begun mapping

---
+++ christies_departments.py
@@ -0,0 +1,27 @@
+
+
+art_departments = {
+66 : "19th century European Art",
+    3: "American Art"
+}
+
+British Art on Paper (1600-1850) 101
+Impressionist & Modern Art 29
+Modern British & Irish Art 99
+Old Master Drawings 69
+Old Master Paintings 70
+Post-War & Contemporary Art 74
+Scottish Art 96
+Sporting & Wildlife Art 50
+Victorian, Pre-Raphaelite & British Impressionist Art 100
+
+(DESIGN)
+19th Century Furniture & Sculpture 68
+American Folk Art & Outsider Art 4
+American Furniture & Decorative Arts 5
+Design 59
+Early European Sculpture & Works of Art 111
+English Furniture & Works of Art 112
+European Ceramics & Glass 25
+European Furniture & Works of Art 26
+Interiors 103
