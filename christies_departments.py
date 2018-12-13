507257a46b52e7929de361d0e932acc8691ecfe3
Marco Meneghelli <mm@bendingspoons.com>
2018-12-13 16:22:47 +0100

    parsed all mapped auctions to category

--- christies_departments.py
+++ christies_departments.py
@@ -1,5 +1,3 @@
-
-
 art_departments = {
     66: "19th century European Art",
     3: "American Art",
@@ -11,7 +9,7 @@ art_departments = {
     74: "Post-War & Contemporary Art",
     96: "Scottish Art",
     50: "Sporting & Wildlife Art",
-    100: "Victorian, Pre-Raphaelite & British Impressionist Art" 
+    100: "Victorian, Pre-Raphaelite & British Impressionist Art"
 }
 
 design_departments = {
@@ -26,3 +24,36 @@ design_departments = {
     103: "Interiors"
 }
 
+
+def department_name_from_id(id):
+    """
+    Function helper to translate id to department human name.
+    """
+
+    if isinstance(id, str):
+        id = int(id)
+    if id in art_departments:
+        return art_departments[id]
+    elif id in design_departments:
+        return design_departments[id]
+    else:
+        return None
+
+
+def category_from_ids(ids):
+    ids = [int(id) if isinstance(id, str) else id for id in ids]
+
+    votes = {'art': 0, 'design': 0}
+
+    for id in ids:
+        if id in art_departments:
+            votes['art'] += 1
+        if id in design_departments:
+            votes['design'] += 1
+
+    if votes['art'] > votes['design']:
+        return 'art'
+    elif votes['art'] == votes['design']:
+        return None
+    else:
+        return 'design'
---
+++ parse_map_auctions.py
@@ -0,0 +1,43 @@
+from os import listdir
+import pandas as pd
+
+from christies_departments import department_name_from_id, category_from_ids
+
+files_destination = "/Users/marcomeneghelli/Dropbox (Personal)/W/Education/tesi Giulia/christies_csvs/departments"
+filename_prefix = "christie_sales_data_department"
+
+auction_map = {}
+
+for f in listdir(files_destination):
+    print(f)
+
+    dep_id = f.replace(filename_prefix, "").replace("_", "").replace(".csv", "")
+    print(dep_id)
+    dep_data = pd.read_csv("{}/{}".format(files_destination, f))
+    # print(dep_data)
+
+    for index, row in dep_data.iterrows():
+        auction_id = row['sale_id']
+        date = row['date']
+
+        # remove date rage from date:
+        if " - " in date:
+            date = date.split(" - ")[1]
+
+        currency = row['currency']
+
+        # print(auction_id, date, currency)
+
+        if auction_id not in auction_map:
+            auction_map[auction_id] = {'date': None, 'currency': None, 'departments_ids': [], 'departments_names': []}
+        auction_map[auction_id]['date'] = date
+        auction_map[auction_id]['currency'] = currency
+        auction_map[auction_id]['departments_ids'].append(dep_id)
+        auction_map[auction_id]['departments_names'].append(department_name_from_id(dep_id))
+    # break
+
+for auction_id in auction_map.keys():
+    auction_map[auction_id]['cartegory'] = category_from_ids(auction_map[auction_id]['departments_ids'])
+
+print(auction_map)
+
