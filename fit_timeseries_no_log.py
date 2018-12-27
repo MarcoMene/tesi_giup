31fcf55ccf86a25dbd996c7af026fb1b188ce341
Marco Meneghelli <mm@bendingspoons.com>
2018-12-27 09:54:49 +0100

    better way to print what you’re looking at in no-log fit

--- fit_timeseries_no_log.py
+++ fit_timeseries_no_log.py
@@ -11,7 +11,6 @@ from scipy.stats import norm
 
 from utils import sigma_to_p_value, dates_to_timedelta_in_years
 
-
 input_file = "/Users/Giulia/bendingspoons/tesi_giup/output_csv/christie_auctions_data.csv"
 
 # sort values by time
@@ -19,29 +18,27 @@ timseries_data = pd.read_csv(input_file)
 timseries_data['date'] = pd.to_datetime(timseries_data.date)
 timseries_data.sort_values(by='date', inplace=True)
 
-
 alpha = 0.01
 confidence = 1 - alpha
 
-
-
-
 # single fit  ** CONFIGURATION
 currency = 'GBP'
-category = 'design' # 'art' #
+category = 'design'  # 'art' #
 
-current_timeseries = timseries_data[ (timseries_data['currency'] == currency) & (timseries_data['category'] == category)]
+current_timeseries = timseries_data[(timseries_data['currency'] == currency) & (timseries_data['category'] == category)]
 current_timeseries.dropna(inplace=True)
 
 # percentuale di lotti invenduti
-#ys = list(  current_timeseries['lots_unsold']/(current_timeseries['lots_unsold'] + current_timeseries['lots_sold']) )
+# ys = list(  current_timeseries['lots_unsold']/(current_timeseries['lots_unsold'] + current_timeseries['lots_sold']) )
 
-#lottiin palio
-ys = list(  current_timeseries['lots_unsold'] + current_timeseries['lots_sold'])
+# lottiin palio
+ys = list(current_timeseries['lots_unsold'] + current_timeseries['lots_sold'])
+
+# *** modify this to title your no-log variable
+what_are_you_looking_at = "lots_unsold + lots_sold"
 
 # take series of timedelta (in years)
 xs = dates_to_timedelta_in_years(current_timeseries['date'])
-
 
 # fit_result = fit_parameters(linear, xs, ys)
 
@@ -52,8 +49,7 @@ estimate_m, sigma_m = m.n, m.s
 
 t_stat = abs(estimate_m) / sigma_m
 
-
-print(" {} - {} - {}".format(currency, category, "percentage of lots unsold"))
+print(" {} - {} - {}".format(currency, category, what_are_you_looking_at))
 
 print("(m , q) : {}".format(fit_result))
 print("t-stat (n-sigma): {}".format(t_stat))
